aebb1d885555c25acfc450529ed888a9b0c7de8a
Haciibrahimli <haci.ibrahimli@gmail.com>
2023-10-12 21:25:51 +0300

    todochanges

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from todo.models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("name", "add_date", "update_date", "status")
+
+
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+CHOICES = (
+    ("Hələ başlanmayıb", "not"),
+    ("Davam edir", "progress"),
+    ("Bitib", "finish")
+)
+
+
+class Todo(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Ediləcək iş")
+    status = models.CharField(max_length=255, choices=CHOICES, verbose_name="Taskin statusu", null=True, blank=True, default="Hələ başlanmayıb")
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    delete_date = models.DateTimeField(null=True, blank=True)
+
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("add_date",)
+        verbose_name = "Ediləcək iş"
+        verbose_name_plural = "Ediləcək işlər"
+
+
+
